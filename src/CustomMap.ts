e79baffe517b17c919140613f769f0be91943a74
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-06-02 07:51:08 +0000

    Add CustomMap class

---
+++ src/CustomMap.ts
@@ -0,0 +1,12 @@
+/// <reference types="@types/google.maps" />
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
+            zoom: 1,
+            center: { lat: 0, lng: 0 },
+        });
+    }
+}
