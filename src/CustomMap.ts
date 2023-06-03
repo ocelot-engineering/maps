fac1f8066b44c0667e2e1bbc9fac92d57e5899c6
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-06-03 07:57:01 +0000

    Add Mappable interface to be implemented by User and Company - add mappable interface - make User and Company classes implement Mappable interface - add addMarker method to CustomMap - add markerContent method to User and Company

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,14 @@
 /// <reference types="@types/google.maps" />
 
+export interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+    markerContent(): string;
+    color: string;
+}
+
 export class CustomMap {
     private googleMap: google.maps.Map;
 
@@ -7,6 +16,24 @@ export class CustomMap {
         this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
             zoom: 1,
             center: { lat: 0, lng: 0 },
+        });
+    }
+
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng,
+            },
+        });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent(),
+            });
+
+            infoWindow.open(this.googleMap, marker);
         });
     }
 }
